2c7ac62111796728ffb0cb78cf1ca5ede2ef4c4e
TrietCao <triet12369@gmail.com>
2023-06-19 21:59:08 -0500

    remove lib gitignore

---
+++ src/module/react-client/src/features/remoteRender/lib/protocols.ts
@@ -0,0 +1,16 @@
+function Cone(session: unknown) {
+  return {
+    createVisualization: () => session.call('vtk.initialize', []),
+    resetCamera: () => session.call('vtk.camera.reset', []),
+    updateResolution: (resolution) =>
+      session.call('vtk.cone.resolution.update', [resolution]),
+    showBoneCT: () => session.call('vtk.dicom3d.presets.bone.ct', []),
+    showAngioCT: () => session.call('vtk.dicom3d.presets.angio.ct', []),
+    showMuscleCT: () => session.call('vtk.dicom3d.presets.muscle.ct', []),
+    showMip: () => session.call('vtk.dicom3d.presets.mip', []),
+    light: () => session.call('vtk.dicom3d.light', []),
+    crop3d: () => session.call('vtk.dicom3d.crop', []),
+  };
+}
+
+export default { Cone };
