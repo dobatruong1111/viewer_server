f71002d552b67324d60a653a1e8f8fdac5a22194
dobatruong <batruongdo3@gmail.com>
2023-07-28 16:10:28 +0700

    updated tool panning

--- viewerserver/module/3dserver/panning/panning_3dobject.py
+++ viewerserver/module/3dserver/panning/panning_3dobject.py
@@ -1,9 +1,12 @@
 import vtk
 from vtkmodules.vtkCommonCore import vtkCommand
 
+from measurement.utils import buildArcAngleMeasurement, buildTextActorLengthMeasurement
+
 class PanningInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
-    def __init__(self):
+    def __init__(self, afterInteractorStyle):
         self.isPanning = False
+        self.afterInteractorStyle = afterInteractorStyle
 
         self.AddObserver(vtkCommand.MouseMoveEvent, self.mouseMoveEvent)
         self.AddObserver(vtkCommand.LeftButtonPressEvent, self.leftButtonPressEvent)
@@ -17,6 +20,17 @@ class PanningInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
 
     def mouseMoveEvent(self, obj: vtk.vtkInteractorStyleTrackballCamera, event: str) -> None:
         if self.isPanning:
+            renderer = self.GetInteractor().GetRenderWindow().GetRenderers().GetFirstRenderer()
+            if self.afterInteractorStyle.getAngleMeasurementPipelines() or self.afterInteractorStyle.getLengthMeasurementPipelines():
+                if self.afterInteractorStyle.getAngleMeasurementPipelines():
+                    for pipeline in self.afterInteractorStyle.getAngleMeasurementPipelines():
+                        points = pipeline.line.GetPoints()
+                        buildArcAngleMeasurement(pipeline.arc, pipeline.textActor, renderer, points)
+                if self.afterInteractorStyle.getLengthMeasurementPipelines():
+                    for pipeline in self.afterInteractorStyle.getLengthMeasurementPipelines():
+                        points = pipeline.line.GetPoints()
+                        buildTextActorLengthMeasurement(pipeline.textActor, renderer, points)
+                self.GetInteractor().Render()
             self.Pan()
         else:
             self.OnMouseMove()
